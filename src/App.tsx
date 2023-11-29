d52ec12622858b00e2db6adc56af27ed80134b3f
wonbae lee <justinlee@unussystems.com>
2023-11-29 16:22:48 +0900

    다크모드, 토글 컨텍스트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState} from "react"
+import { useEffect, useState, useContext} from "react"
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged  } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
+import ThemeContext from "context/ThemeContext";
 import "react-toastify/dist/ReactToastify.css";
 
 import Router from './components/Router';
 import Loader from "components/Loader";
 export default function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
 // auth를 체크하기 전에 (initialize 전) loader를 띄워주는 용도
   const [init, setInit] = useState<boolean>(false);
@@ -23,9 +25,9 @@ export default function App() {
     });
   }, [auth])
   return (
-    <>
+    <div className={context.theme==="light"?"white":"dark"}>
     <ToastContainer/>
     {init? <Router isAuthenticated={isAuthenticated}/> : <Loader/>}
-    </>
+    </div>
   );
 }
