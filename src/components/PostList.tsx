f4c673f57d78f4586a13a9a1a15f7b43148eb163
wonbae lee <justinlee@unussystems.com>
2023-11-29 16:58:32 +0900

    댓글 쓰기, 삭제(배열의 업데이트)

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -11,6 +11,13 @@ interface PostListProps {
 }
 type TabType = "all" | "my"
 
+export interface CommentsInterface {
+  content: string;
+  uid: string;
+  email: string;
+  createdAt: string;
+}
+
 export interface PostProps {
   id?: string
   title: string
@@ -21,6 +28,7 @@ export interface PostProps {
   updatedAt?: string
   uid: string
   category?: CategoryType
+  comments?: CommentsInterface[];
 }
 export type CategoryType = "Frontend" | "Backend" | "Web" | "Native";
 export const CATEGORIES: CategoryType[] = ["Frontend", "Backend", "Web", "Native"]
